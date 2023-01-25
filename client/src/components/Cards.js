cea137626ed128775e041addf695147eb391ed9a
Pankaj Barnwal <suno.pankaj@gmail.com>
2023-01-25 10:29:03 +0530

    chore/deleted card-setting.js

--- client/src/components/Cards.js
+++ client/src/components/Cards.js
@@ -1,9 +1,11 @@
 import HeadingCard from './HeadingCard';
+import QuestionCard from './QuestionCard';
 
 const Cards = () => {
 	return (
-		<div className="bg-lightBlue h-screen mt-4 p-4 flex flex-col items-center">
+		<div className="bg-lightBlue h-screen mt-4 p-4 flex flex-col items-center gap-4">
 			<HeadingCard />	
+			<QuestionCard />
 		</div>
 	)
 }
