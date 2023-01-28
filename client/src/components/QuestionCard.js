e46b6a986cf335a508f2d490ea5a846a7c58c201
Pankaj Barnwal <suno.pankaj@gmail.com>
2023-01-28 09:00:48 +0530

    feat/making form element in controlled in react

--- client/src/components/QuestionCard.js
+++ client/src/components/QuestionCard.js
@@ -1,8 +1,31 @@
-import { RiQuestionAnswerLine } from 'react-icons/ri';
-import { BsTextParagraph } from 'react-icons/bs';
 import Inputs from './Inputs';
+import { useState } from 'react';
+import { removeItem } from '../utilities/removeItem';
+// import { BsTextParagraph } from 'react-icons/bs';
+// import { RiQuestionAnswerLine } from 'react-icons/ri';
 
 const QuestionCard = () => {
+	const [checkItems, setCheckItems] = useState(['Item 1', 'Item 2', 'Item 3', 'Others']);
+	const [questionText, setQuestionText] = useState('Question');
+	const [selected, setSelected] = useState('checkbox');
+
+	const handleInputChange = (event) => {
+		console.log(event.target.value);
+		setQuestionText(event.target.value);
+	}
+
+	const handleChange = (event) => {
+		console.log(event.target.value);
+		setSelected(event.target.value);
+	}
+
+	const handleAbort = (item) => {
+		// console.log(checkItems);
+		const newCheckItems = removeItem(checkItems, item);
+		setCheckItems(newCheckItems);
+		// console.log(checkItems);
+	}
+
 	return (
 		<>
 			<div className="rounded-lg py-3 w-3/6 bg-white shadow-2xl text-center">
@@ -10,20 +33,30 @@ const QuestionCard = () => {
 					<input 
 						className="text-sm col-span-2 outline-0 border-b border-b-lightBlue font-normal mx-4 mb-3 focus:border-none"
 						type="text" 
-						defaultValue="Question"
+						value={questionText}
+						onChange={handleInputChange}
 					/>
-					<select className="border text-sm mr-2 p-2 rounded" id="question" name="question">
-						<option value="short"><RiQuestionAnswerLine />Short Answer</option>
-						<option value="paragraph"><BsTextParagraph /> Paragraph</option>
+					<select 
+						className="border text-sm mr-2 p-2 rounded" 
+						id="question" 
+						name="question"
+						value={selected}
+						onChange={handleChange}
+					>
+						<option value="short">Short Answer</option>
+						<option value="paragraph">Paragraph</option>
 						<option value="multiple">Multiple Choice</option>
-						<option selected value="checkbox">CheckBox</option>
+						<option value="checkbox">CheckBox</option>
 						<option value="dropdown">Dropdown</option>
 						<option value="linear">Linear Scale</option>
 						<option value="date">Date</option>
 						<option value="time">Time</option>
 					</select>
 				</div>
-				<Inputs />
+				<Inputs 
+					checkItems={checkItems}
+					handleAbort={handleAbort}
+				/>
 			</div>
 		</>
 	)
