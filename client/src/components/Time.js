becbb9df2af29b3f4b747c1598d5632e9a6b3b12
Pankaj Barnwal <suno.pankaj@gmail.com>
2023-01-30 15:31:26 +0530

    feat/boiler plate for remaining input type

---
+++ client/src/components/Time.js
@@ -0,0 +1,21 @@
+const Time = () => {
+	return (
+		<div className="ml-4 mt-2 flex flex-col items-start">
+			<div
+				className="flex justify-between items-center w-full"
+				key="text"
+			>
+				<div>
+					<input 
+						className="text-sm col-span-2 outline-0 border-b border-b-lightBlue font-normal ml-1 mb-3 focus:border-none"
+						type="text"
+						value="text"
+						name="text"
+					/>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default Time;
