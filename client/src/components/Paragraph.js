b1927b910957db56ed79682d6e1016838590ca5b
Pankaj Barnwal <suno.pankaj@gmail.com>
2023-02-02 08:40:40 +0530

    feat/adding TextareaAutosize to allow autogrow in textarea input

--- client/src/components/Paragraph.js
+++ client/src/components/Paragraph.js
@@ -1,3 +1,5 @@
+import TextareaAutosize from 'react-textarea-autosize';
+
 const Paragraph = ({ paragraph, handleParaChange }) => {
 	return (
 		<div className="ml-4 mt-2 flex flex-col items-start">
@@ -6,9 +8,8 @@ const Paragraph = ({ paragraph, handleParaChange }) => {
 				key="text"
 			>
 				<div className="w-full mr-4">
-					<textarea 
-						className="w-full border-0 no-scrollbar overflow-auto text-sm col-span-2 resize-none outline-0 border-b border-b-lightBlue font-normal mx-1 mb-3 focus:border-none"
-						type="text"
+					<TextareaAutosize 
+						className="w-full border-0 no-scrollbar overflow-auto text-sm resize-none outline-0 border-b border-b-lightBlue font-normal mx-1 mb-3"
 						value={paragraph}
 						onChange={handleParaChange}
 						name="text"
