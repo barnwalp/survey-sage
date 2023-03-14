c6febcfbbc250a73f068b13e96656f344cac5edf
Pankaj Barnwal <suno.pankaj@gmail.com>
2023-03-14 20:40:18 +0530

    feat/creating state using react and accessing in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,12 @@ import { useState } from 'react';
 import Navigation from './components/Navigation';
 import Cards from './components/Cards';
 import PreviewState from './context/previewState';
+import { useSelector, useDispatch } from 'react-redux';
 
 export default function App() {
 	const [title, setTitle] = useState('Untitled Form');
+	const count = useSelector((state) => state.counter.count);
+	console.log(`count is from reducer: ${count}`)
 
 	const handleTitle = (event) => {
 		console.log(event.target.value);
