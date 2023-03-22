a8ec46680c870590a2b7d37277b463a41db3e49a
Pankaj Barnwal <suno.pankaj@gmail.com>
2023-03-22 22:09:11 +0530

    feat/added form schema

---
+++ server/src/models/forms.mongo.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const formSchema = new mongoose.Schema({
+	id: {
+		type: Number,
+		required: true,
+	},
+	input: {
+		type: String,
+		required: true,
+	},
+	value: {
+		type: [ String ],
+		required: true
+	},
+	answer: {
+		type: String,
+		required: true,
+	},
+})
+
+module.exports = mongoose.model('Form', formSchema);
