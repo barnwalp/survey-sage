b30d1edd4edde1bfd77243219ebdff1e9d97436b
Pankaj Barnwal <suno.pankaj@gmail.com>
2023-03-31 08:31:53 +0530

    feat/creating forms data file

---
+++ server/src/data/forms.js
@@ -0,0 +1,50 @@
+let count = 100;
+
+const forms = [
+	{
+		id: count++,
+		input: 'short',
+		value: ['Write your answer'],
+		answer: '',
+	},
+	{
+		id: count++,
+		input: 'paragraph',
+		value: ['Write your paragraph'],
+		answer: '',
+	},
+	{
+		id: count++,
+	 	input: 'checkbox',
+	 	value: ['Item 1', 'Item 2', 'Item 3', 'Other'],
+		answer: '',
+	},
+	{
+		id: count++,
+	 	input: 'multiple',
+	 	value: ['Item 1', 'Item 2', 'Item 3'],
+		answer: '',
+	},
+	{
+		id: count++,
+	 	input: 'dropdown',
+	 	value: ['Item 1', 'Item 2', 'Item 3'],
+		answer: '',
+	},
+	{
+		id: count++,
+	 	input: 'date',
+	 	value: 'Day, Month, Year',
+		answer: '',
+	},
+	{
+		id: count++,
+	 	input: 'time',
+	 	value: 'Time',
+		answer: '',
+	},
+]
+
+module.exports = {
+	forms,
+}
